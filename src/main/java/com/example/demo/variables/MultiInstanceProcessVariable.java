a9b5895b4d20b151861a72156bb626238b7d0ffb
Johann Helbig <johann.helbig@camunda.com>
2023-08-23 18:26:27 +0200

    seems to be working

---
+++ src/main/java/com/example/demo/variables/MultiInstanceProcessVariable.java
@@ -0,0 +1,10 @@
+package com.example.demo.variables;
+
+import lombok.Data;
+
+@Data
+public class MultiInstanceProcessVariable {
+    private Integer by2;
+    private Integer by3;
+    private Integer by4;
+}
--- src/main/java/com/example/demo/worker/Worker.java
+++ src/main/java/com/example/demo/worker/Worker.java
@@ -1,8 +1,12 @@
 package com.example.demo.worker;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
+
+import com.example.demo.variables.MultiInstanceProcessVariable;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
@@ -10,22 +14,38 @@ import io.camunda.zeebe.spring.client.annotation.JobWorker;
 @Component
 public class Worker {
 
+    private ObjectMapper mapper = new ObjectMapper();
+
     @JobWorker
-    public Map<String, Integer> multiplyBy2(final ActivatedJob job) {
-        Integer element = (Integer)job.getVariablesAsMap().get("element");
-        return Map.of("result.2", element * 2);
+    public Map<String, MultiInstanceProcessVariable> createResult(final ActivatedJob job) {
+        return Map.of("result", new MultiInstanceProcessVariable());
     }
 
     @JobWorker
-    public Map<String, Integer> multiplyBy3(final ActivatedJob job) {
-        Integer element = (Integer)job.getVariablesAsMap().get("element");
-        return Map.of("result.3", element * 3);
+    public Map<String, MultiInstanceProcessVariable> multiplyBy2(final ActivatedJob job) {
+        Integer element = (Integer) job.getVariablesAsMap().get("element");
+        MultiInstanceProcessVariable result = mapper.convertValue(job.getVariablesAsMap().get("result"),
+                MultiInstanceProcessVariable.class);
+        result.setBy2(element * 2);
+        return Map.of("result", result);
     }
 
     @JobWorker
-    public Map<String, Integer> multiplyBy4(final ActivatedJob job) {
-        Integer element = (Integer)job.getVariablesAsMap().get("element");
-        return Map.of("result.4", element * 4);
+    public Map<String, MultiInstanceProcessVariable> multiplyBy3(final ActivatedJob job) {
+        Integer element = (Integer) job.getVariablesAsMap().get("element");
+        MultiInstanceProcessVariable result = mapper.convertValue(job.getVariablesAsMap().get("result"),
+                MultiInstanceProcessVariable.class);
+        result.setBy3(element * 3);
+        return Map.of("result", result);
+    }
+
+    @JobWorker
+    public Map<String, MultiInstanceProcessVariable> multiplyBy4(final ActivatedJob job) {
+        Integer element = (Integer) job.getVariablesAsMap().get("element");
+        MultiInstanceProcessVariable result = mapper.convertValue(job.getVariablesAsMap().get("result"),
+                MultiInstanceProcessVariable.class);
+        result.setBy4(element * 4);
+        return Map.of("result", result);
     }
 
 }
